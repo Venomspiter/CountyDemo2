5f896819a82ea287de7681cd1e30a46f49668ffe
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-02 09:49:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,12 +12,15 @@ def render_main():
 def main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    print(alphabetically_first_county(counties)) 
    
 def get_state_options(counties):
+    curState = counties[0]['state']
     options = ""
-    for s in counties:
-        options += Markup("<option value=\"" + s + "\">" + s + "</option>")
+    for state in counties:
+        if state["State"] != curState:
+            s = state["State"]
+            options += Markup("<option value=\"" + s + "\">" + s + "</option>")
+            curState = state["State"]
     return options
 def Markup(str):
     

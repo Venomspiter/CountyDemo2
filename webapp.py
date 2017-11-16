6278394b7ff9d726139bad0e96a45f9bb43d5cff
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-16 09:39:03 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,28 +1,33 @@
-from flask import Flask, request, Markup, render_template, flash, Markup
-import os
+
+from flask import Flask, render_template, request, Markup, flash, Markup
+import os 
 import json
 
-app = Flask(__name__)
-        
-def main():
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-   
-def get_state_options(counties):
-    curState = counties[0]['state']
-    options = ""
-    for state in counties:
-        if state["State"] != curState:
-            s = state["State"]
-            options += Markup("<option value=\"" + s + "\">" + s + "</option>")
-            curState = state["State"]
-    return render_template('index.html', stateBlock = options())
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
-    #return render_template('index.html', stateBlock = get_state_options(), stateFact = state_Fact())
     return get_state_options()
+
     
+@app.route("/app")
+def render_fact():
+        state = request.args['options']
+         counties = json.load(demographics_data)
+    return render_template('index.html', stateFact =  state_Fact(state, counties), option = get_state_options())
+    
+
+def get_state_options():
+        with open('county_demographics.json') as demographics_data:
+            counties = json.load(demographics_data)
+        options = ""
+        state = ""
+            for c in counties:
+             if not c["State"] == state:
+                options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+            state = c["State"]
+    return render_template('index.html', stateBlock = options)
+
 """def state_Fact(state, counties):
     total = 0
      for c in counties:

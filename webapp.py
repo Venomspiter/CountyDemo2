cffaeaae7309e57bfc1e85615c72ae825eb0e1bf
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-16 10:13:39 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,11 +2,7 @@
 from flask import Flask, render_template, request, Markup, flash, Markup
 import os 
 import json
-
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
-
-
-    
 @app.route("/app")
 def render_fact():
     state = request.args['options']

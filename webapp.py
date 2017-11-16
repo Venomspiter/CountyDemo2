357d12663a384d3bb2bf36f18d1cb30eb4f23d8d
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-16 09:12:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,6 +3,9 @@ import os
 import json
 
 app = Flask(__name__)
+if __name__== '__main__':
+    main()
+    app.run(debug=False, port=54321)
         
 def main():
     with open('county_demographics.json') as demographics_data:
@@ -30,7 +33,3 @@ def render_main():
                 total = total + c['Population']['2014 Population']
      return "The total number of people living in this state in 2014 was " + total + " people."
      """
-    
-if __name__== '__main__':
-    main()
-    app.run(debug=False, port=54321)

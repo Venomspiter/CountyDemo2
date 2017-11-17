e651e0b203c9c7a72be879332b505891372b9460
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-17 09:31:28 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, Markup, flash, Markup
 import os 
 import json
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
-@app.route("/app")
 def render_fact():
     state = request.args['options']
     return render_template('index.html', stateFact =  state_Fact(state), option = get_state_options())
@@ -22,7 +21,7 @@ def get_state_options():
 
 @app.route("/")
 def render_main():
-    return get_state_options()
+    return render_fact()
 
 def state_Fact(state):
     with open('county_demographics.json') as demographics_data:
